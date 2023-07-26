1ee3d588b1df1486a39180c5ff6a6fc8a5ed5d3f
Nikita <nikita.shushevich@gmail.com>
2023-07-26 13:07:38 +0300

    first test added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,19 +5,21 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 abstract public class BaseTest {
-public void setUp(){
-    WebDriverManager.chromedriver().setup();
-    Configuration.browser = "chrome";
-    Configuration.driverManagerEnabled = true;
-    Configuration.browserSize = "1920x1080";
-    Configuration.headless = false; //for browser visibility during test run
-}
-@BeforeTest
-    public void init(){
-    setUp();
-}
-@AfterTest
-    public void tearDown(){
-    Selenide.closeWebDriver();
-}
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
+        Configuration.driverManagerEnabled = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.headless = false; //for browser visibility during test run
+    }
+
+    @BeforeTest
+    public void init() {
+        setUp();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
 }
