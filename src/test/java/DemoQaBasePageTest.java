2fa6db730c61fdcb6c94c89d6716220064099a4b
Nikita <nikita.shushevich@gmail.com>
2023-07-27 16:21:15 +0300

    new tests added

--- src/test/java/DemoQaBasePageTest.java
+++ src/test/java/DemoQaBasePageTest.java
@@ -1,6 +1,13 @@
 import Pages.BasePage.BasePage;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.logging.SocketHandler;
 
 public class DemoQaBasePageTest extends BaseTest {
 
@@ -45,5 +52,24 @@ public class DemoQaBasePageTest extends BaseTest {
         basePage.clickOnBookStoreAppButton();
         Assert.assertEquals(basePage.getBookStoreAppPageHeaderText().getText(), "Book Store");
     }
+    @Test
+    public void checkFooterText(){
+        BasePage basePage = new BasePage(BASE_URL);
+        Assert.assertEquals(basePage.getBasePageFooterText().getText(), "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.");
+    }
 
-}
+    @Test
+    public void numberOfElementsCheck(){
+        BasePage basePage = new BasePage(BASE_URL);
+        basePage.clickOnBookStoreAppButton();
+        ElementsCollection booksListItems = basePage.getListOfBooks();
+        System.out.println(booksListItems.size());
+        Assert.assertEquals(booksListItems.size(), 8);
+        }
+
+
+
+    }
+
+
+
